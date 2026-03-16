ac1c42e43076694f4bd4354e9d66581a7eba9087
Ketan Choyal <ketanchoyal@gmail.com>
2026-03-16 14:18:58 -0400

    feat(dart): extend platform interface with camera methods

--- lib/waffle_camera_plugin_platform_interface.dart
+++ lib/waffle_camera_plugin_platform_interface.dart
@@ -1,5 +1,8 @@
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
+import 'src/camera_description.dart';
+import 'src/recording_state.dart';
+import 'src/resolution_preset.dart';
 import 'waffle_camera_plugin_method_channel.dart';
 
 abstract class WaffleCameraPluginPlatform extends PlatformInterface {
@@ -8,7 +11,8 @@ abstract class WaffleCameraPluginPlatform extends PlatformInterface {
 
   static final Object _token = Object();
 
-  static WaffleCameraPluginPlatform _instance = MethodChannelWaffleCameraPlugin();
+  static WaffleCameraPluginPlatform _instance =
+      MethodChannelWaffleCameraPlugin();
 
   /// The default instance of [WaffleCameraPluginPlatform] to use.
   ///
@@ -25,5 +29,54 @@ abstract class WaffleCameraPluginPlatform extends PlatformInterface {
 
   Future<String?> getPlatformVersion() {
     throw UnimplementedError('platformVersion() has not been implemented.');
+  }
+
+  /// Retrieves a list of available cameras on the device.
+  Future<List<CameraDescription>> getAvailableCameras() {
+    throw UnimplementedError('getAvailableCameras() has not been implemented.');
+  }
+
+  /// Creates a camera instance with the given description and resolution preset.
+  ///
+  /// Returns the camera ID for use in subsequent operations.
+  Future<int> createCamera(CameraDescription camera, ResolutionPreset preset) {
+    throw UnimplementedError('createCamera() has not been implemented.');
+  }
+
+  /// Initializes the camera with the given ID.
+  Future<void> initializeCamera(int cameraId) {
+    throw UnimplementedError('initializeCamera() has not been implemented.');
+  }
+
+  /// Starts recording video from the camera with the given ID.
+  Future<void> startRecording(int cameraId) {
+    throw UnimplementedError('startRecording() has not been implemented.');
+  }
+
+  /// Stops recording and returns the file path of the saved video.
+  Future<String> stopRecording(int cameraId) {
+    throw UnimplementedError('stopRecording() has not been implemented.');
+  }
+
+  /// Pauses recording on the camera with the given ID.
+  Future<void> pauseRecording(int cameraId) {
+    throw UnimplementedError('pauseRecording() has not been implemented.');
+  }
+
+  /// Resumes recording on the camera with the given ID.
+  Future<void> resumeRecording(int cameraId) {
+    throw UnimplementedError('resumeRecording() has not been implemented.');
+  }
+
+  /// Disposes the camera with the given ID, freeing resources.
+  Future<void> disposeCamera(int cameraId) {
+    throw UnimplementedError('disposeCamera() has not been implemented.');
+  }
+
+  /// Returns a stream of recording state changes for the camera with the given ID.
+  Stream<RecordingState> onRecordingStateChanged(int cameraId) {
+    throw UnimplementedError(
+      'onRecordingStateChanged() has not been implemented.',
+    );
   }
 }
